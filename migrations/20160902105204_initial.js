b567b352268600028300667a94aa791016576fa2
Samantha Spensley <spenslsm@gmail.com>
2016-09-02 11:17:53 -0600

    migrations created for taco_toppings table

--- migrations/20160902105204_initial.js
+++ migrations/20160902105204_initial.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('taco_toppings', (table) => {
+    table.increments();
+    table.string('name');
+    table.timestamps();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('initial')
 };
